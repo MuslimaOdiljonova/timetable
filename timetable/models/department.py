c83560232e717ed7be661a5816c6c3586205df68
Lilly <x.ezoza06@gmail.com>
2026-05-02 09:11:42 +0500

    first commit

---
+++ timetable/models/department.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.conf import settings
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
